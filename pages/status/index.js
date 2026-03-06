16954d81fe5469af4e6582f266d31b818e0fe603
Lucas Iniesta Simoes <l.iniesta.94@gmail.com>
2026-03-06 18:43:37 -0300

    fix: fix new fetching bug

--- pages/status/index.js
+++ pages/status/index.js
@@ -17,21 +17,25 @@ export default function StatusPage() {
 }
 
 function DatabaseStatus() {
-  const { data, isLoading } = useSWR("/api/v1/status", fetchAPI, {
+  const { data, isLoading, error } = useSWR("/api/v1/status", fetchAPI, {
     refreshInterval: 2000,
   });
 
   const loading = "Carregando...";
+
+  if (isLoading || (!data && !error)) {
+    return <div>{loading}</div>;
+  }
+
+  if (error) {
+    return <div>Falha ao carregar dados do sitema.</div>;
+  }
 
   const updatedAtText = new Date(data.updated_at).toLocaleString("pt-BR");
   const databaseVersion = data.dependencies.database.version;
   const databaseOpenedConnections =
     data.dependencies.database.opened_connections;
   const databaseMaxConnections = data.dependencies.database.max_connections;
-
-  if (isLoading && !data) {
-    return <div>{loading}</div>;
-  }
 
   return (
     <>
